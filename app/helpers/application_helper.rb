90f406f0dbf6d4424cb7e4e94fc4486ce73995b1
Yuta Ikawa <yutaikawa0315@gmail.com>
2018-01-17 20:15:59 +0900

    ページタイトル　リファクタリング

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+	# ページ毎のタイトルを返す
+	def full_title(page_title = '')
+		base_title = "FootBall広場"
+		if page_title.empty?
+			base_title
+		else
+			page_title + " | " + base_title
+		end
+	end
+
 end
