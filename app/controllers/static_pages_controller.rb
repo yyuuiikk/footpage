7aa08a15614436ad86e8556e9ebfea723d731e89
Yuta Ikawa <yutaikawa0315@gmail.com>
2018-01-17 18:45:58 +0900

    各ページ毎のページネーション実装

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,18 +3,18 @@ class StaticPagesController < ApplicationController
 	end
 
 	def premier
-		@articles = Article.where(category: 0)
-	end
-
-	def bundes
-		@articles = Article.where(category: 1)
+		@articles = Article.where(category: 0).paginate(page: params[:page], per_page: 9)
 	end
 
 	def liga
-		@articles = Article.where(category: 2)
+		@articles = Article.where(category: 1).paginate(page: params[:page], per_page: 9)
+	end
+
+	def bundes
+		@articles = Article.where(category: 2).paginate(page: params[:page], per_page: 9)
 	end
 
 	def serie
-		@articles = Article.where(category: 3)
+		@articles = Article.where(category: 3).paginate(page: params[:page], per_page: 9)
 	end
 end
