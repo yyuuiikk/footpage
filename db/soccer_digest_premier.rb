cf1b62efa3f8c509ee169b1c7cb6582c9a085007
Yuta Ikawa <yutaikawa0315@gmail.com>
2018-01-17 12:32:38 +0900

    cronプログラム調整

--- db/soccer_digest_premier.rb
+++ db/soccer_digest_premier.rb
@@ -19,8 +19,9 @@ doc = Nokogiri::HTML.parse(html, nil, charset)
 doc.xpath('//div[@class="entry"]').each do |node|
 	# 1記事ずつ取得するために配列を初期化
 	site_info = []
-	# サムネイル/記事タイトル/記事リンク/サイト名/サイトトップのURL の取得
-	site_info << site_top_url + node.css('img').attribute('src').value \
+	# 日付/サムネイル/記事タイトル/記事リンク/サイト名/サイトトップのURL の取得
+	site_info << node.css('span[class="date"]').inner_text.gsub(/([年月])/, "-").gsub(/(日)/, "") \
+						<< site_top_url + node.css('img').attribute('src').value \
 						<< node.css('p[class="title"]').inner_text \
 						<< node.css('p[class="title"]/a').attribute('href').value \
 						<< doc.css('link[rel="home"]').attribute('title').value \
