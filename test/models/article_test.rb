3029b62ec71b55cba2a4cf35ca934143f7455da8
Yuta Ikawa <yutaikawa0315@gmail.com>
2018-01-18 13:18:43 +0900

    モデルテスト作成

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	def setup
+		@article = Article.new(date: "2018-01-18", thumnail: "http://img.com", title: "テストタイトル", link: "http://example.com/link", site_name: "テストサイト", site_top: "http://testsite.com/", category: 0)
+	end
+
+	test "should be valid" do
+		assert @article.valid?
+	end
+
 end
