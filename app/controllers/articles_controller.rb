08e8659b08a6de7fc2d312146ecf5efb7b0219a9
Yuta Ikawa <yutaikawa0315@gmail.com>
2018-01-18 13:46:10 +0900

    ヘッダーリンクのテスト

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,3 @@
 class ArticlesController < ApplicationController
-	def index
-		@articles = Article.paginate(page: params[:page], per_page: 9)
-	end
 
 end
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -13,4 +13,21 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
 		assert_select "a[href=?]", serie_path
 	end
 
+	# ヘッダーリンクの表示テスト
+	test "header link should be valid" do
+		get premier_path
+		assert_select ".dropdown-menu a[href=?]", premier_path
+		assert_select ".dropdown-menu a[href=?]", bundes_path
+		assert_select ".dropdown-menu a[href=?]", liga_path
+		assert_select ".dropdown-menu a[href=?]", serie_path
+	end
+
+	test "header link should not be valid" do
+		get root_path
+		assert_select ".dropdown-menu a[href=?]", premier_path, count: 0
+		assert_select ".dropdown-menu a[href=?]", bundes_path, count: 0
+		assert_select ".dropdown-menu a[href=?]", liga_path, count: 0
+		assert_select ".dropdown-menu a[href=?]", serie_path, count: 0
+	end
+
 end
