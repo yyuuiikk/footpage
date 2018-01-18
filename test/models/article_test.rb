76974c71a5356c4729905d4039ce40bf6a5482db
Yuta Ikawa <yutaikawa0315@gmail.com>
2018-01-18 13:39:02 +0900

    fixture使用

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class ArticleTest < ActiveSupport::TestCase
 
 	def setup
-		@article = Article.new(date: "2018-01-18", thumnail: "http://img.com", title: "テストタイトル", link: "http://example.com/link", site_name: "テストサイト", site_top: "http://testsite.com/", category: 0)
+		@article = articles(:one)
 	end
 
 	test "should be valid" do
