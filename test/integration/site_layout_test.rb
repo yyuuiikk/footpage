50bf1064a57d9d4539cd687465da20cb2fadc906
Yuta Ikawa <yutaikawa0315@gmail.com>
2018-01-18 13:48:24 +0900

    ヘッダーリンクのテスト 全ページ確認

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -14,14 +14,6 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
 	end
 
 	# ヘッダーリンクの表示テスト
-	test "header link should be valid" do
-		get premier_path
-		assert_select ".dropdown-menu a[href=?]", premier_path
-		assert_select ".dropdown-menu a[href=?]", bundes_path
-		assert_select ".dropdown-menu a[href=?]", liga_path
-		assert_select ".dropdown-menu a[href=?]", serie_path
-	end
-
 	test "header link should not be valid" do
 		get root_path
 		assert_select ".dropdown-menu a[href=?]", premier_path, count: 0
@@ -30,4 +22,35 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
 		assert_select ".dropdown-menu a[href=?]", serie_path, count: 0
 	end
 
+	test "header link should be valid -premier-" do
+		get premier_path
+		assert_select ".dropdown-menu a[href=?]", premier_path
+		assert_select ".dropdown-menu a[href=?]", bundes_path
+		assert_select ".dropdown-menu a[href=?]", liga_path
+		assert_select ".dropdown-menu a[href=?]", serie_path
+	end
+
+	test "header link should be valid -bundes-" do
+		get bundes_path
+		assert_select ".dropdown-menu a[href=?]", premier_path
+		assert_select ".dropdown-menu a[href=?]", bundes_path
+		assert_select ".dropdown-menu a[href=?]", liga_path
+		assert_select ".dropdown-menu a[href=?]", serie_path
+	end
+
+	test "header link should be valid -liga-" do
+		get liga_path
+		assert_select ".dropdown-menu a[href=?]", premier_path
+		assert_select ".dropdown-menu a[href=?]", bundes_path
+		assert_select ".dropdown-menu a[href=?]", liga_path
+		assert_select ".dropdown-menu a[href=?]", serie_path
+	end
+
+	test "header link should be valid -serie-" do
+		get serie_path
+		assert_select ".dropdown-menu a[href=?]", premier_path
+		assert_select ".dropdown-menu a[href=?]", bundes_path
+		assert_select ".dropdown-menu a[href=?]", liga_path
+		assert_select ".dropdown-menu a[href=?]", serie_path
+	end
 end
