c04016e976a5dde7a0f5b9086d3704218a0da06a
Yuta Ikawa <yutaikawa0315@gmail.com>
2018-01-18 16:36:44 +0900

    記事の並びを日付順に修正

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,18 +3,18 @@ class StaticPagesController < ApplicationController
 	end
 
 	def premier
-		@articles = Article.where(category: 0).paginate(page: params[:page], per_page: 9)
+		@articles = Article.where(category: 0).order('date DESC').paginate(page: params[:page], per_page: 9)
 	end
 
 	def liga
-		@articles = Article.where(category: 1).paginate(page: params[:page], per_page: 9)
+		@articles = Article.where(category: 1).order('date DESC').paginate(page: params[:page], per_page: 9)
 	end
 
 	def bundes
-		@articles = Article.where(category: 2).paginate(page: params[:page], per_page: 9)
+		@articles = Article.where(category: 2).order('date DESC').paginate(page: params[:page], per_page: 9)
 	end
 
 	def serie
-		@articles = Article.where(category: 3).paginate(page: params[:page], per_page: 9)
+		@articles = Article.where(category: 3).order('date DESC').paginate(page: params[:page], per_page: 9)
 	end
 end
